4d080f94b3ed7aa9cd694087826fe089224eceb4
Kazotka <80603582+Kazotka@users.noreply.github.com>
2023-11-10 18:24:04 -0500

    Ajout d'un 'handler' d'events

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const { join } = require('path');
+const { readdirSync } = require('fs');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-Events.ClientReady ? console.log('L\'application c\'est lancer !') : console.log('Il y a eu un souci lors du démarrage de l\'application!?');
+const eventsPath = join(__dirname, 'events');
+const eventFiles = readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-require('dotenv').config();
+for (const file of eventFiles) {
+  const filePath = join(eventsPath, file);
+  const event = require(filePath);
+  event.once ? client.once(event.name, (...args) => event.execute(...args)) : client.on(event.name, (...args) => event.execute(...args));
+
+}
+
 client.login(process.env.token);
