5e4555bbac365b89bf673058ac68eef2ebd38402
Kazotka <80603582+Kazotka@users.noreply.github.com>
2023-12-08 19:02:03 -0500

    Update de l'event handler

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ require('dotenv').config();
 
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const eventsHandler = require('./functions/eventsHandler');
-// const commandsHandler = require('./functions/commandsHandler');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 client.events = new Collection();
-// client.commands = new Collection();
+client.commands = new Collection();
 
 eventsHandler(client);
-// commandsHandler(client);
 
-client.login(process.env.token);
+const { token } = require('./config.json');
+client.login(token);
